e15697f512c661cd7fab9740f962aa4428e0518a
hamediseyedali <hamediseyedali@gmail.com>
2024-08-10 09:29:07 +0330

    News model added

--- technews/news/models.py
+++ technews/news/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.contrib.postgres.fields import ArrayField
 
-# Create your models here.
+
+class News(models.Model):
+    title = models.CharField(max_length=100)
+    body = models.TextField()
+    tags = ArrayField(models.CharField(max_length=100),
+                      blank=True, default=list)
+    source = models.CharField(max_length=100)
