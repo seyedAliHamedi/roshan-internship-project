c3182deded7003efb6f96db83a8abe11d21defd6
hamediseyedali <hamediseyedali@gmail.com>
2024-08-10 09:39:17 +0330

    Views implemented

--- technews/news/views.py
+++ technews/news/views.py
@@ -1,3 +1,41 @@
+from rest_framework import generics
+from .models import News
+from .serializer import NewsSerializer
 from django.shortcuts import render
 
-# Create your views here.
+
+class NewsView(generics.ListAPIView):
+    serializer_class = NewsSerializer
+    queryset = News.objects.all()
+
+
+class NewsDetailView(generics.RetrieveAPIView):
+    serializer_class = NewsSerializer
+    queryset = News.objects.all()
+
+
+class CreateNewsView(generics.CreateAPIView):
+    serializer_class = NewsSerializer
+    queryset = News.objects.all()
+
+
+class UpdateNewsView(generics.UpdateAPIView):
+    serializer_class = NewsSerializer
+    queryset = News.objects.all()
+
+
+class DeleteNewsView(generics.DestroyAPIView):
+    serializer_class = NewsSerializer
+    queryset = News.objects.all()
+
+
+class GetNewsByTagsView(generics.ListAPIView):
+    serializer_class = NewsSerializer
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        tags = self.request.query_params.get('tags', None)
+        if tags:
+            tags_list = tags.split(',')
+            queryset = queryset.filter(tags__overlap=tags_list)
+        return queryset
